e2ece0a8612d1b3125160f68c43e8047995e6177
MrHamshed01 <135401447+MrHamshed01@users.noreply.github.com>
2023-06-03 11:14:48 +0530

    Update start.py

--- AsuXMusic/Modules/start.py
+++ AsuXMusic/Modules/start.py
@@ -92,18 +92,4 @@ async def start(_, message: Message):
     )
 
 
-@Abishnoi.on_message(filters.command(["repo", "source"]))
-async def help(client: Abishnoi, message: Message):
-    await message.reply_photo(
-        photo=f"https://te.legra.ph/file/56557bd94afbe895ae483.jpg",
-        caption=f"""ʜᴇʀᴇ ɪs ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ғᴏʀᴋ ᴀɴᴅ ɢɪᴠᴇ sᴛᴀʀs ✨""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        " ʀᴇᴘᴏ ⚒️", url=f"https://github.com/Abishnoi69/AsuXMusic"
-                    )
-                ]
-            ]
-        ),
-    )
+        
